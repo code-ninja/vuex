e61d20c388cd2669f9a8042dacc85f39d54f88b3
Paulo Marinas <paulopmx@gmail.com>
2017-08-07 10:56:31 +0800

    disable auto install in dist mode for esm file

--- dist/vuex.esm.js
+++ dist/vuex.esm.js
@@ -668,9 +668,9 @@ function install (_Vue) {
 }
 
 // auto install in dist mode
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
-}
+// if (typeof window !== 'undefined' && window.Vue) {
+//   install(window.Vue);
+// }
 
 var mapState = normalizeNamespace(function (namespace, states) {
   var res = {};
